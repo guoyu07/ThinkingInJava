cf3d239787208dcca305a6d11793ca59ded0f8a0
hechao <hhhhhhhelloworld@gmail.com>
2016-04-19 20:49:26 +0800

    init

---
+++ ThinkingInJava/src/com/hechao/day1/第一章对象导论.java
@@ -0,0 +1,10 @@
+package com.hechao.day1;
+
+public class 第一章对象导论 {
+
+	public static void main(String[] args) {
+		System.out.println("第一章对象导论");
+		System.out.println(new 第一章对象导论().toString());
+	}
+
+}
---
+++ ThinkingInJava/src/com/hechao/day2/Test_Javadoc.java
@@ -0,0 +1,35 @@
+
+//: object/Test_Javadoc.java
+//object.Documentation1.java
+//TIJ4 Chapter Object, Exercise 13 - 2
+/* Run Documentation1.java, Documentation2.java and Documentation3.java 
+* through Javadoc. Verify the resulting documentation with your Web browser. 
+*/
+package com.hechao.day2;
+
+import java.util.*;
+
+//object/Documentation2.java
+/**
+* <pre>
+* Uses
+* System.out.println(new Date());
+* </pre>
+*/ 
+
+public class Test_Javadoc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+	
+}
+
+
+/*
+ Output:
+ hello 
+*///:~
